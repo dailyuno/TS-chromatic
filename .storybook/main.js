f29d887d05311f288784358ab02c5a59aa332310
Private Uno <rndnsghEjr3525@gmail.com>
2022-12-09 18:30:10 +0900

    chore: turbosnap 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,26 @@
+const turbosnap = require("vite-plugin-turbosnap");
+const { mergeConfig } = require("vite");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins:
+        configType === "PRODUCTION"
+          ? [turbosnap({ rootDir: config.root ?? process.cwd() })]
+          : [],
+    });
+  },
+};
